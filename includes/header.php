58417ec3e0f605cba112f0531bc38c8d1345dae8
NovaKid3011 <mayorleanni@gmail.com>
2024-06-17 04:23:21 +0800

    update

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@ $activeTab = isset($_GET['tab']) ? $_GET['tab'] : '1';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="../../VoyaVerseProject/css/header.css">
+  <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
   
