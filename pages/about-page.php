05fcaf430e64603ab401c3af935115bddb7ffb9b
NovaKid3011 <mayorleanni@gmail.com>
2024-06-17 06:44:24 +0800

    update

---
+++ pages/about-page.php
@@ -0,0 +1,51 @@
+<html>
+
+<section class="about-us">
+  <div class="container">
+    <h2>About Us</h2>
+    <p>Exceptional travel experiences crafted with precision and creativity</p>
+  </div>
+</section>
+
+<section class="our-story">
+  <div class="container">
+    <h2>Our Story</h2>
+    <p>Consulting represents success at realizing the company is going in the wrong direction. The only time the company fails is when it is not possible to do a turnaround anymore. We help companies pivot into more profitable directions where they can expand and grow. It is inevitable that companies will end up making a few mistakes; we help them correct these mistakes.</p>
+  </div>
+</section>
+
+<section class="statistics">
+  <div class="container">
+    <div class="stat">
+      <h3>378</h3>
+      <p>Tour has done successfully</p>
+    </div>
+    <div class="stat">
+      <h3>30</h3>
+      <p>Yearly tour arrange</p>
+    </div>
+    <div class="stat">
+      <h3>2263</h3>
+      <p>Happy Clients</p>
+    </div>
+  </div>
+</section>
+
+<section class="testimonials">
+  <div class="container">
+    <div class="testimonial">
+      <p>"Working in conjunction with humanitarian aid agencies, we have supported programmes to help alleviate human suffering."</p>
+      <h4>- Micky Mouse</h4>
+    </div>
+    <div class="testimonial">
+      <p>"Working in conjunction with humanitarian aid agencies, we have supported programmes to help alleviate human suffering."</p>
+      <h4>- Tom Mouse</h4>
+    </div>
+    <div class="testimonial">
+      <p>"Working in conjunction with humanitarian aid agencies, we have supported programmes to help alleviate human suffering."</p>
+      <h4>- Jerry Mouse</h4>
+    </div>
+  </div>
+</section>
+
+</html>
