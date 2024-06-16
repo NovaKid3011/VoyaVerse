1c375047dd6f3282d46d1bb15436c947b39dcdec
NovaKid3011 <mayorleanni@gmail.com>
2024-06-17 07:48:58 +0800

    update

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/index.css">
-  <title>VoyaVerse</title>
-</head>
-<body>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>VoyaVerse</title>
+  </head>
+  <body>
 
 <?php
 include_once 'includes/message.php';
